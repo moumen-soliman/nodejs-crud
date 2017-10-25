ed3488b27944d9211efa6cad89c8d4fec9e08ae4
moumensoliman <moumensoliman@gmail.com>
2017-10-25 02:39:33 +0200

    add base lines

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-//empty-file
+const port = process.env.PORT || 4300,
+      express = require('express'),
+	  app = express(),
+	  expressLayouts = require('express-ejs-layouts'),
+      expressValidator = require('express-validator');
+      session        = require('express-session'),
+	  mongoose       = require('mongoose'),
+      bodyParser     = require('body-parser'),
+      cookieParser   = require('cookie-parser'),
+      flash          = require('connect-flash');
+
+app.listen(port, () => {
+	console.log(`port is listening on ${port}`);
+});
+
+module.exports = {app};
