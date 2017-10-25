5dc5775812331ae781ee5befca3a888f2ce61f90
moumensoliman <moumensoliman@gmail.com>
2017-10-25 03:27:03 +0200

    require and set main middleware functions

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
+//to use .env file
+require('dotenv').config();
+
 const port = process.env.PORT || 4300,
       express = require('express'),
-	  app = express(),
-	  expressLayouts = require('express-ejs-layouts'),
+	    app = express(),
+	    expressLayouts = require('express-ejs-layouts'),
       expressValidator = require('express-validator');
       session        = require('express-session'),
-	  mongoose       = require('mongoose'),
+	    mongoose       = require('mongoose'),
       bodyParser     = require('body-parser'),
       cookieParser   = require('cookie-parser'),
       flash          = require('connect-flash');
 
+//add cookie parser
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET, 
+  cookie: { maxAge: 60000 },
+  resave: false,    
+  saveUninitialized: false  
+}));
+app.use(flash());
+
+//make public is static directorey
+app.use(express.static(__dirname + '/public'));
+
+//make ejs is main for views , acutally i want to use hbs, but try something different
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+//body-parser for data form
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+
+//port listen
 app.listen(port, () => {
 	console.log(`port is listening on ${port}`);
 });
