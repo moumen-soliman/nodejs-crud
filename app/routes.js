01fe1775c84ffa48d915496200953dc9fd6c1a6b
moumensoliman <moumensoliman@gmail.com>
2017-10-27 02:19:33 +0200

    add routes in routes file

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const express = require('express'),
 	  router = express.Router(),
 	  homeController = require('./controllers/home.controller');
+  	  eventsController = require('./controllers/events.controller');
 
 module.exports = router;
 
@@ -8,9 +9,17 @@ router.get('/', homeController.displayHome); // to display and get homepage
 
 router.get('/events', eventsController.showEvents);
 
+router.get('/events/:slug', eventsController.showSingle);
+
 router.get('/events/create',  eventsController.showCreate);
 router.post('/events/create', eventsController.processCreate);
 
 router.get('/events/edit',  eventsController.showEdit);
 router.post('/events/edit', eventsController.processEdit);
 
+router.get('/events/:slug/edit', eventsController.showEdit);
+router.post('/events/:slug',     eventsController.processEdit);
+
+router.get('/events/:slug/delete', eventsController.deleteEvent);
+
+router.get('/events/seed',  eventsController.seedEvents);
