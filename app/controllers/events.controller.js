37ce542e0e4d85e797a790f785eb9d946ed6e944
moumensoliman <moumensoliman@gmail.com>
2017-10-27 02:01:53 +0200

    create function process and show

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -17,3 +17,49 @@ function showEvents (req, res){
 		});
 	});
 }
+
+function showSingle(req, res){
+	Event.findOne({slug: req.params.slug}, (err, event) => {
+		if(err) {
+			res.status(404);
+			res.send('Event not found');
+		}
+
+		res.render('pages/single', {
+			event: event,
+			success: req.flash('success')
+		});
+	});
+}
+
+function showCreate(req, res) {
+  res.render('pages/create', {
+    errors: req.flash('errors')
+  });
+}
+
+
+function processCreate(req, res){
+	req.CheckBody('name', 'Name is required').notEmpty();
+	req.CheckBody('description', 'description is required').notEmpty();
+
+	const errors = req.validationErrors();
+	if (errors) {
+		req.flash('errors', errors.map(err => err.msg));
+		return res.redirect('/events/create');
+	}
+
+	const event = new Event({
+		name: req.body.name,
+		description: req.body.description
+	});
+
+	event.save((err) => {
+		if (err)
+			throw err;
+
+		req.flash('success', 'Successfuly created event !');
+
+		res.redirect(`/events/${event.slug}`);
+	});
+}
--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,7 @@ module.exports = router;
 router.get('/', homeController.displayHome); // to display and get homepage
 
 router.get('/events', eventsController.showEvents);
+
+router.get('/events/create',  eventsController.showCreate);
+router.post('/events/create', eventsController.processCreate);
+
